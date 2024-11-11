b2fb24596582c9b700cf8cf8089432c61077634d
wada <wada0421514@gmail.com>
2024-11-11 21:32:56 +0900

    add admin user to setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE IF NOT EXISTS `xsuportal` DEFAULT CHARACTER SET utf8mb4;
 CREATE USER IF NOT EXISTS `isucon`@`localhost` IDENTIFIED WITH mysql_native_password BY 'isucon';
+CREATE USER IF NOT EXISTS `admin`@`%` IDENTIFIED WITH mysql_native_password BY 'admin';
 GRANT ALL ON `xsuportal`.* TO `isucon`@`localhost`;
+GRANT ALL ON `xsuportal`.* TO `admin`@`%`;
